e1939a969d81d2fdabcf55d1963808c8f7a73590
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2022-06-14 21:55:34 +0900

    Revert "Merge branch 'main' of https://github.com/go2665/atents220516"
    This reverts commit 67fef09b85ed5e1f3c5ec78fe247bd53bdb850a5, reversing
    changes made to 7cf7b123467fde07a05bd756ab194b6c5e44cc48.

--- 03_3DBasic/Assets/Script/Player.cs
+++ 03_3DBasic/Assets/Script/Player.cs
@@ -1,75 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-    public float turnSpeed = 180.0f;
-    PlayerInputActions actions = null;
-    Vector3 inputDir = Vector3.zero;
-    float inputSide = 0.0f;
-
-    Rigidbody rigid = null;
-    Animator anim = null;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        actions = new PlayerInputActions();
-        rigid = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
-    }
-
-    private void OnEnable()
-    {
-        actions.Player.Enable();
-        actions.Player.Move.performed += OnMoveInput;
-        actions.Player.Move.canceled += OnMoveInput;
-        actions.Player.SideMove.performed += OnSideMoveInput;
-        actions.Player.SideMove.canceled += OnSideMoveInput;
-    }    
-
-    private void OnDisable()
-    {
-        actions.Player.SideMove.canceled -= OnSideMoveInput;
-        actions.Player.SideMove.performed -= OnSideMoveInput;
-        actions.Player.Move.canceled -= OnMoveInput;
-        actions.Player.Move.performed -= OnMoveInput;   
-        actions.Player.Disable();
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-        Rotate();        
-    }
-
-    void Move()
-    {
-        // inputDir의 y값을 이용하여 이 오브젝트의 앞쪽 방향(transform.forward)으로 이동
-        // inputSide를 이용해서 이 오브젝트의 오른쪽 방향(transform.right)으로 이동
-        rigid.MovePosition(rigid.position
-            + moveSpeed * Time.fixedDeltaTime * (inputDir.y * transform.forward + inputSide * transform.right));
-    }
-
-    void Rotate()
-    {
-        // inputDir.x를 이용하여 우회전(d,+1)인지 좌회전(a,-1)인지 결정. 회전의 중심축
-        rigid.MoveRotation(rigid.rotation * Quaternion.AngleAxis(inputDir.x * turnSpeed * Time.fixedDeltaTime, transform.up));
-    }
-
-    private void OnMoveInput(InputAction.CallbackContext context)
-    {
-        //Debug.Log(context.ReadValue<Vector3>());
-        inputDir = context.ReadValue<Vector2>();    // Vector2.x = a키(-1) d키(+1),  Vector2.y = w키(+1) s키(-1)
         
-        anim.SetBool("IsMove", !context.canceled);
     }
 
-    private void OnSideMoveInput(InputAction.CallbackContext context)
+    // Update is called once per frame
+    void Update()
     {
-        //Debug.Log(context.ReadValue<float>());
-        inputSide = context.ReadValue<float>();
-
-        anim.SetBool("IsMove", !context.canceled);
+        
     }
 }
